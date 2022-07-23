90a78a92849e814e621a8912337a3925cbbc44a2
akshata <akshata@toucan-l001>
2022-07-23 21:41:31 +0530

    yes

---
+++ maps/src/maps/Maps.java
@@ -0,0 +1,17 @@
+package maps;
+
+import java.util.HashMap;
+
+public class Maps {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HashMap<Integer,String> map= new HashMap<>();   
+    map.put(1,"hi");   
+    map.put(2, "This");   
+    map.put(3,"is");   
+    map.put(4,"java");   
+    System.out.println(map);    
+	}
+
+}
